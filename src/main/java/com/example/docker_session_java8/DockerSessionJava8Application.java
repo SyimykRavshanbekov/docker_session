361eef010984dce4ef8a54e1687ca36471c364c5
SyimykRavshanbekov <programmist00707@gmail.com>
2023-05-27 19:02:03 +0600

    add mvc controller

--- src/main/java/com/example/docker_session_java8/DockerSessionJava8Application.java
+++ src/main/java/com/example/docker_session_java8/DockerSessionJava8Application.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DockerSessionJava8Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DockerSessionJava8Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DockerSessionJava8Application.class, args);
+    }
 
 }
--- src/main/java/com/example/docker_session_java8/controller/ApiController.java
+++ src/main/java/com/example/docker_session_java8/controller/ApiController.java
@@ -1,4 +1,15 @@
 package com.example.docker_session_java8.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/api")
 public class ApiController {
+
+    @GetMapping("/hello")
+    public String helloMethod() {
+        return "index";
+    }
 }
